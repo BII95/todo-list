89b40af7c7177eeb06cffeaedfa8076b04af8f69
Bryan Iturbide <87789203+BII95@users.noreply.github.com>
2026-08-19 15:01:55 -0500

    toggle between todo Display n edit

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,14 +1,31 @@
-function TodoListItem({todo,onCompleteTodo}){
-return(
-        <li>
-            <input
-                type="checkbox"
-                checked={todo.isCompleted}
-                onChange={() => onCompleteTodo(todo.id)}
-            />
-                {todo.title}
-        </li>
-    );
-}
+import { useState } from "react";
+import TextInputWithLabel from "../../shared/TextInputWithLabel";
 
-export default TodoListItem;
+export default function TodoListItem({todo,onCompleteTodo}){
+    const [isEditing,setIsEditing]=useState(false)
+    return(
+        <li>
+            <form>
+                {isEditing ? ( 
+                    <TextInputWithLabel
+                        value = {todo.title}            
+                    />
+                ) : (
+                        <>
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    id={`checkbox${todo.id}`}
+                                    checked={todo.isCompleted}
+                                    onChange={() => onCompleteTodo(todo.id)}
+                                />
+                            </label>
+                            <span onClick={() => setIsEditing(true)}>
+                                    {todo.title}
+                            </span>
+                        </>
+                )}
+            </form>
+        </li>           
+        );
+}
