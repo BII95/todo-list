ab681ff01bff8fc576d34e65a65483681b184ed6
Bryan Iturbide <87789203+BII95@users.noreply.github.com>
2026-08-19 15:13:50 -0500

    helper function

--- src/features/TodoForm.jsx
+++ src/features/TodoForm.jsx
@@ -1,6 +1,6 @@
 import { useRef,useState } from "react";
 import TextInputWithLabel from "../shared/TextInputWithLabel";
-
+import{isValidTodoTitle} from "../utils/todoValidation"
 
 function TodoForm({onAddTodo}) {
     const inputRef = useRef();
@@ -29,8 +29,7 @@ function TodoForm({onAddTodo}) {
             />
             <button 
                 type="submit"
-                disabled=
-                {!workingTodoTitle.trim()}
+                disabled={!isValidTodoTitle(workingTodoTitle)}
              >
                 Add Todo
             </button>
