2cba48bc153f540fd5aa05e9298582c40974d717
Bryan Iturbide <87789203+BII95@users.noreply.github.com>
2026-08-26 18:56:11 -0500

    logon component

--- src/features/Logon.jsx
+++ src/features/Logon.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import TextInputWithLabel from "../shared/TextInputWithLabel";
 export default function Logon ({onSetEmail,onSetToken}){
     const [email,setEmail]=useState('')
+    const [password, setPassword] = useState('')
     const [authError,setAuthError]=useState('')
-    const [isLogginOn,setIsLoggingOn]=useState(false)
-    async function handleSubmit() {
+    const [isLoggingOn,setIsLoggingOn]=useState(false)
+    async function handleSubmit(event) {
         try{ 
+            setIsLoggingOn(true)
             const response = await fetch('/api/users/logon', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
@@ -13,8 +16,10 @@ export default function Logon ({onSetEmail,onSetToken}){
             });
             const data = await response.json();
             if (response.status === 200 && data.name && data.csrfToken) {
+                event.preventdefault();
                 onSetEmail(data.name);
                 onSetToken(data.csrfToken);
+
             } else {
                 setAuthError(`Authentication failed: ${data?.message}`);
             }
@@ -24,6 +29,28 @@ export default function Logon ({onSetEmail,onSetToken}){
                 setIsLoggingOn(false);
             }
     }
-    function NewComponent({ newProp = () => {} })
-    function NewComponent({ newProp = () => {} })
+    return (
+        <form onSubmit={handleSubmit}>
+            {/* <TextInputWithLabel
+            ></TextInputWithLabel> */}
+            <label htmlFor="email">Email</label>
+            <input 
+                id="email"
+                type="text"
+                value={email}
+                onSubmit={handleSubmit}
+                required
+            />
+            <label htmlFor="password">Password</label>
+            <input 
+                id="password"
+                type="text"
+                value={password}
+                onSubmit={handleSubmit}
+                required
+            />
+            <button type="submit">Submit</button>
+
+        </form>
+    )
 }
