c51a9c1c62dea85c14b93fbbfefc274cb50037cc
Bryan Iturbide <87789203+BII95@users.noreply.github.com>
2026-08-26 15:14:42 -0500

    moved from app to todospage

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,43 @@
 import  './App.css';
-import TodoList from './features/Todos/TodoList/TodoList.jsx';
-import TodoForm from './features/Todos/TodoForm.jsx';
-import { useState } from 'react';
+// import TodoList from './features/Todos/TodoList/TodoList.jsx';
+// import TodoForm from './features/Todos/TodoForm.jsx';
+// import { useState } from 'react';
 
 function App() {
-  const [todoList,setTodoList]=useState([])
+  // const [todoList,setTodoList]=useState([])
   
-  function addTodo(todoTitle){
-    let newTodo = {id: Date.now(),
-                      title: todoTitle,
-                    isCompleted: false}
-    setTodoList(previous => [newTodo,...previous])
-  }
+  // function addTodo(todoTitle){
+  //   let newTodo = {id: Date.now(),
+  //                     title: todoTitle,
+  //                   isCompleted: false}
+  //   setTodoList(previous => [newTodo,...previous])
+  // }
 
-  function completeTodo(id){
-    const updatedTodos = todoList.map(todo => {
-        if (todo.id === id){
-          const newObj = { ...todo, isCompleted: true}
-          return newObj
-        } else {
-          return todo
-        }
-        }
-      );
+  // function completeTodo(id){
+  //   const updatedTodos = todoList.map(todo => {
+  //       if (todo.id === id){
+  //         const newObj = { ...todo, isCompleted: true}
+  //         return newObj
+  //       } else {
+  //         return todo
+  //       }
+  //       }
+  //     );
 
-      setTodoList(updatedTodos)
-    }
+  //     setTodoList(updatedTodos)
+  //   }
     
-    function updateTodo(editedTodo){
-      const updatedTodos2 = todoList.map(todo => {
-        if(todo.id === editedTodo.id){
-          return{ ...editedTodo}; 
-        }else {
-          return todo
-        }
-      }
-    );
-      setTodoList(updatedTodos2)
-    }
+  //   function updateTodo(editedTodo){
+  //     const updatedTodos2 = todoList.map(todo => {
+  //       if(todo.id === editedTodo.id){
+  //         return{ ...editedTodo}; 
+  //       }else {
+  //         return todo
+  //       }
+  //     }
+  //   );
+  //     setTodoList(updatedTodos2)
+  //   }
   return (
     <div>
       <h1>Todo List</h1>
---
+++ src/features/Todos/TodosPage.jsx
@@ -0,0 +1,41 @@
+import TodoList from './features/Todos/TodoList/TodoList.jsx';
+import TodoForm from './features/Todos/TodoForm.jsx';
+import { useState } from 'react';
+
+export default function TodosPage(){
+      const [todoList,setTodoList]=useState([])
+      
+      function addTodo(todoTitle){
+        let newTodo = {id: Date.now(),
+                          title: todoTitle,
+                        isCompleted: false}
+        setTodoList(previous => [newTodo,...previous])
+      }
+    
+      function completeTodo(id){
+        const updatedTodos = todoList.map(todo => {
+            if (todo.id === id){
+              const newObj = { ...todo, isCompleted: true}
+              return newObj
+            } else {
+              return todo
+            }
+            }
+          );
+    
+          setTodoList(updatedTodos)
+        }
+        
+        function updateTodo(editedTodo){
+          const updatedTodos2 = todoList.map(todo => {
+            if(todo.id === editedTodo.id){
+              return{ ...editedTodo}; 
+            }else {
+              return todo
+            }
+          }
+        );
+          setTodoList(updatedTodos2)
+        }
+    
+}
