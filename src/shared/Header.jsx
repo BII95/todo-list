6d6ae488590357c8d60e4a162b0d52a7f8c7cbc3
Bryan Iturbide <87789203+BII95@users.noreply.github.com>
2026-08-27 16:37:16 -0500

    ignore unused props

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,4 +1,5 @@
-export default function Header({token,onSetEmail,onSetToken}){
+/* eslint-disable-next-line no-unused-vars */
+export default function Header({_token,_onSetEmail,_onSetToken}){
     return(
         <h1>
             Todo List
