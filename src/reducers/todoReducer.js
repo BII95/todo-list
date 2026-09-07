8f0c574b892ee80143a75d4b3a8af06a3fb10319
Bryan Iturbide <87789203+BII95@users.noreply.github.com>
2026-09-06 20:48:14 -0500

    todoreducer intro

---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,12 @@
+export const TODO_ACTIONS = {
+    //FETCHES
+    FETCH_START: 'FETCH_START',
+    FETCH_SUCCESS: 'FETCH_SUCCESS',
+    FETCH_ERROR: 'FETCH_ERROR',
+    
+    // Add todo operations
+    ADD_TODO_START: 'ADD_TODO_START',
+    ADD_TODO_SUCCESS: 'ADD_TODO_SUCCESS',
+    ADD_TODO_ERROR: 'ADD_TODO_ERROR',
+  // ... continue for all operations
+}
