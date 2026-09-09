8add97db77f7a4ffcb9958deb2c455c1b32aa45b
Bryan Iturbide <87789203+BII95@users.noreply.github.com>
2026-09-09 12:39:47 -0500

    start pt 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import  './App.css';
+import { Routes,Route } from 'react-router';
+
 
 
 import Header from './shared/Header';
-import TodosPage from './features/Todos/TodosPage';
-import Logon from './features/Logon.jsx';
-import { useAuth } from './contexts/AuthContext.jsx';
+// import TodosPage from './features/Todos/TodosPage';
+// import Logon from './features/Logon.jsx';
+// import { useAuth } from './contexts/AuthContext.jsx';
 function App() {
-  const{isAuthenticated}=useAuth();
+  // const{isAuthenticated}=useAuth();
   return (
     <div>
       <Header/>
-      {isAuthenticated ?
-        <TodosPage/> : <Logon />}
+      <Routes>
+        {/*routes will go here*/}
+      </Routes>
+      {/* {isAuthenticated ?
+        <TodosPage/> : <Logon />} */}
     </div>
   );
 }
