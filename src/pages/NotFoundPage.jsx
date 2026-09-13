a798ff22190922403e34d5370f0fcaa7e5e42f8a
Bryan Iturbide <87789203+BII95@users.noreply.github.com>
2026-09-12 22:21:56 -0500

    Prettier NotfoundPage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,21 +1,22 @@
-import {Link} from 'react-router';
+import { Link } from 'react-router';
 
-export default function NotFoundPage(){
-   return ( <div> 
-                    <h1>404 - Page Not Found</h1> 
-                    <p> Sorry, the page you're looking for doesn't exist. </p> 
+export default function NotFoundPage() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p> Sorry, the page you're looking for doesn't exist. </p>
 
-                    <ul>
-                        <li>
-                                <Link to= "/">Go Home</Link></li>
-                        <li>
-                                <Link to= "/login">Back to login</Link>
-
-                        </li>
-                        <li>
-                                <Link to= "/about">About</Link>
-                        </li>                        
-                     </ul>
-            </div> 
-                    );
+      <ul>
+        <li>
+          <Link to="/">Go Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Back to login</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
