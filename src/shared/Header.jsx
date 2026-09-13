d955b3b1e29a803416913362beee0aeddbaa6778
Bryan Iturbide <87789203+BII95@users.noreply.github.com>
2026-09-13 16:12:40 -0500

    applied css module to header

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,14 +1,17 @@
 import { useAuth } from '../contexts/AuthContext';
 import Logoff from '../features/Logoff';
 import Navigation from './Navigation';
+import styles from '../styles/Header.module.css';
 
 export default function Header() {
   const { isAuthenticated } = useAuth();
   return (
-    <div>
-      <h1>Todo List</h1>
-      <Navigation></Navigation>
-      {isAuthenticated && <Logoff />}
+    <div className={styles.header}>
+      <div className={styles.nav}>
+        <h1>Todo List</h1>
+        <Navigation></Navigation>
+        {isAuthenticated && <Logoff />}
+      </div>
     </div>
   );
 }
