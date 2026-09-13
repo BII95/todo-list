b51ed70956f28ec4156fdcd401d2f196e083739f
Bryan Iturbide <87789203+BII95@users.noreply.github.com>
2026-09-12 22:00:49 -0500

    Prettier RequireAuth.jsx

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -1,24 +1,23 @@
-import { useLocation,useNavigate } from "react-router";
-import { useEffect } from "react";
-import { useAuth } from "../contexts/AuthContext";
+import { useLocation, useNavigate } from 'react-router';
+import { useEffect } from 'react';
+import { useAuth } from '../contexts/AuthContext';
 
-export default function RequireAuth ({children}){
-    const{isAuthenticated}=useAuth();
-    const location = useLocation();
-    const navigate = useNavigate();
-    useEffect(() => {
-
+export default function RequireAuth({ children }) {
+  const { isAuthenticated } = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
     if (!isAuthenticated) {
-      navigate("/login", 
-        {replace: true ,
-        state:{
+      navigate('/login', {
+        replace: true,
+        state: {
           from: location,
         },
-    });
+      });
     }
   }, [isAuthenticated, navigate, location]);
-    if (!isAuthenticated){
-        return<p>checking authentication...</p>
-    }
-    return children;
+  if (!isAuthenticated) {
+    return <p>checking authentication...</p>;
+  }
+  return children;
 }
