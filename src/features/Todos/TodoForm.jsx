deffdfd629460c2eb91f1beaf849e1c5013c09a6
Bryan Iturbide <87789203+BII95@users.noreply.github.com>
2026-09-12 22:14:56 -0500

    prettier TodoForm.jsx

--- src/features/Todos/TodoForm.jsx
+++ src/features/Todos/TodoForm.jsx
@@ -1,39 +1,33 @@
-import { useRef,useState } from "react";
-import TextInputWithLabel from "../../shared/TextInputWithLabel";
-import {isValidTodoTitle} from "../../utils/todoValidation"
+import { useRef, useState } from 'react';
+import TextInputWithLabel from '../../shared/TextInputWithLabel';
+import { isValidTodoTitle } from '../../utils/todoValidation';
 
-function TodoForm({onAddTodo}) {
-    const inputRef = useRef();
-    const [workingTodoTitle,setTodoTitle]=useState('');
-    const handleInputChange = (event) => {
-        setTodoTitle(event.target.value)
-    }
-    const handleAddTodo = (event) => {
-        event.preventDefault();
-        onAddTodo(workingTodoTitle);
-        inputRef.current.focus();
-        setTodoTitle('')
-    }
+function TodoForm({ onAddTodo }) {
+  const inputRef = useRef();
+  const [workingTodoTitle, setTodoTitle] = useState('');
+  const handleInputChange = (event) => {
+    setTodoTitle(event.target.value);
+  };
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    onAddTodo(workingTodoTitle);
+    inputRef.current.focus();
+    setTodoTitle('');
+  };
 
-
-    
-    
-    return (
-        <form onSubmit={handleAddTodo}>
-            <TextInputWithLabel
-                elementId="todoTitle"
-                ref={inputRef}
-                value = {workingTodoTitle}
-                onChange={handleInputChange}
-                labelText="Todo"            
-            />
-            <button 
-                type="submit"
-                disabled={!isValidTodoTitle(workingTodoTitle)}
-             >
-                Add Todo
-            </button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleAddTodo}>
+      <TextInputWithLabel
+        elementId="todoTitle"
+        ref={inputRef}
+        value={workingTodoTitle}
+        onChange={handleInputChange}
+        labelText="Todo"
+      />
+      <button type="submit" disabled={!isValidTodoTitle(workingTodoTitle)}>
+        Add Todo
+      </button>
+    </form>
+  );
 }
 export default TodoForm;
