9594d8b505ee2c6ca65875c449d5c38c90ba4682
Bryan Iturbide <87789203+BII95@users.noreply.github.com>
2026-09-12 22:12:50 -0500

    prettier Logon.jsx

--- src/features/Logon.jsx
+++ src/features/Logon.jsx
@@ -1,48 +1,44 @@
-import { useState } from "react";
-import { useAuth } from "../contexts/AuthContext";
+import { useState } from 'react';
+import { useAuth } from '../contexts/AuthContext';
 
-
-export default function Logon (){
-    const {login} = useAuth();
-    const [email,setEmail]=useState('')
-    const [password, setPassword] = useState('')
-    const [authError,setAuthError]=useState('')
-    const [isLoggingOn,setIsLoggingOn]=useState(false)
-    async function handleSubmit(event) {
-            event.preventDefault();
-            setAuthError('')
-            setIsLoggingOn(true)
-            const result = await login(email, password);
-            if (!result.success) {
-               setAuthError(result.error);
-            }
-            setIsLoggingOn(false);
-            
+export default function Logon() {
+  const { login } = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [authError, setAuthError] = useState('');
+  const [isLoggingOn, setIsLoggingOn] = useState(false);
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setAuthError('');
+    setIsLoggingOn(true);
+    const result = await login(email, password);
+    if (!result.success) {
+      setAuthError(result.error);
     }
-    return (
-        <form onSubmit={handleSubmit}>
-            {authError && <p>{authError}</p>}
-            <label htmlFor="email">Email</label>
-            <input 
-                id="email"
-                type="text"
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
-                required
-            />
-            <label htmlFor="password">Password</label>
-            <input 
-                id="password"
-                type="password"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
-                required
-            />
-            <button type="submit"
-                    disabled={isLoggingOn}>
-                        {isLoggingOn ? "Logging in..." : "Log On"}
-                        
-            </button>
-        </form>
-    )
+    setIsLoggingOn(false);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      {authError && <p>{authError}</p>}
+      <label htmlFor="email">Email</label>
+      <input
+        id="email"
+        type="text"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+        required
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        type="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+        required
+      />
+      <button type="submit" disabled={isLoggingOn}>
+        {isLoggingOn ? 'Logging in...' : 'Log On'}
+      </button>
+    </form>
+  );
 }
